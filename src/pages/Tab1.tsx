515710d45823822581e10f8ff312260263d6cecb
Maksim Krivoshapko <freulaeux@freulaeux-osx.yandex.ru>
2024-03-13 23:17:35 +0300

    add tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import React,  { useState } from 'react';
+// @ts-ignore
+import factory from 'ggwave';
 
 const Tab1: React.FC = () => {
+  const [text, setText] = useState('');
+
+  function convertTypedArray(src: any, type: any) {
+    var buffer = new ArrayBuffer(src.byteLength);
+    var baseView = new src.constructor(buffer).set(src);
+    return new type(buffer);
+  }
+
+  const handlePlayAudio = () => {
+    factory().then(function(ggwave: any) {
+      var context = new AudioContext({sampleRate: 48000});
+      var parameters = ggwave.getDefaultParameters();
+      var instance = ggwave.init(parameters);
+      var payload = text;
+      var waveform = ggwave.encode(instance, payload, ggwave.ProtocolId.GGWAVE_PROTOCOL_AUDIBLE_NORMAL, 10);
+      var buf = convertTypedArray(waveform, Float32Array);
+      var buffer = context.createBuffer(1, buf.length, context.sampleRate);
+      buffer.getChannelData(0).set(buf);
+      var source = context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(context.destination);
+      source.start(0);
+  });
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Text to Audio</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent fullscreen className="ion-text-center ion-padding">
+      <div className="centered-content">
+        <IonInput value={text} placeholder="Enter text" onIonChange={e => setText(e.detail.value!)} />
+        <IonButton onClick={handlePlayAudio}>Play Audio</IonButton>
+      </div>
       </IonContent>
     </IonPage>
   );
